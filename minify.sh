7d6cfc21313fe32631085bf557e76d44483d28bf
Charles Law <claw@conduce.com>
2020-05-26 23:08:19 -0700

    Use minification on index

--- minify.sh
+++ minify.sh
@@ -1,5 +1,5 @@
 
-for folder in projects about contact; do
+for folder in . projects about contact; do
     echo $folder
     html-minifier --use-short-doctype --collapse-whitespace --remove-comments --use-short-doctype --minify-js --minify-css --output ${folder}/index.html ${folder}/.src.html
 done
